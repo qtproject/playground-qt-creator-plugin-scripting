26476d2bd113da46c08bb99cb298f923267be88f
Jesper K. Pedersen <jesper.pedersen@kdab.com>
2013-05-15 10:46:54 +0200

    make it possible to compare arrays from the C++ side
    Change-Id: I3cb0ae4c3648a4c61e5e2bbe1cca442814ee5161
    Reviewed-by: Nicolas Arnaud-Cormos <nicolas@kdab.com>

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,7 @@
 var TEST_JS_LOADED // Purposfully left undefined - this is to avoid the global variables being loaded more than once
 if (TEST_JS_LOADED == undefined) {
     TEST_JS_LOADED = true
+
     // Set to true to print out every single comparison
     var verbose = false
 
@@ -25,19 +26,38 @@ if (TEST_JS_LOADED == undefined) {
         }
     }
 
-    function compare(act, exp, message) {
+    function logCompare(act,exp,message) {
         if ( verbose ) {
             if ( message != undefined )
                 console.log("comparing(" + message + ") " + act + " with " + exp)
             else
                 console.log("comparing " + act + " with " + exp)
         }
-        if (act != exp) {
-            if ( message != undefined )
-                error("Compare failed (" + message +"): actual=" + act + " expected=" + exp)
-            else
-                error("Compare failed: actual=" + act + " expected=" + exp)
+    }
+
+    function failCompare(act,exp,message) {
+        if ( message != undefined )
+            error("Compare failed (" + message +"): actual=" + act + " expected=" + exp)
+        else
+            error("Compare failed: actual=" + act + " expected=" + exp)
+    }
+
+    function compareArrays(act, exp, message) {
+        logCompare(act,exp,message)
+        if (act.length != exp.length)
+            failCompare(act,exp,message)
+        for (var i=0; i<act.length; ++i) {
+            if (act[i] != exp[i]) {
+                failCompare(act,exp,message)
+                break
+            }
         }
+    }
+
+    function compare(act, exp, message) {
+        logCompare(act,exp,message)
+        if (act != exp)
+            failCompare(act,exp,message)
     }
 
     function comparePositions(act, exp, message) {
